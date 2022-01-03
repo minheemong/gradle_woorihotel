d9c214b2acefc21c286ff916391e5dde05a51000
mong <mong@DESKTOP-S41GB3I>
2022-01-03 21:41:49 +0900

    first

---
+++ src/main/java/com/ezen/spg/WooriApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.spg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezen/spg/WooriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.spg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
