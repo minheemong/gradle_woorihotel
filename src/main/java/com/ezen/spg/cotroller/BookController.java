51efe2db7e7da0bdf15b8b780043a750ad87a647
k <k@DESKTOP-RRGCEUV>
2022-01-04 17:34:17 +0900

    복구

--- src/main/java/com/ezen/spg/cotroller/BookController.java
+++ src/main/java/com/ezen/spg/cotroller/BookController.java
@@ -4,10 +4,5 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookController {
-	// hihi ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ
-	// 그렇구나 수정하는 건 파란색
 	
-	public String orderss() {
-		return "";
-	}
 }
