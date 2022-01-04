7269704f0d3821abc2bb93cf6ee5e484a02c34df
k <k@DESKTOP-RRGCEUV>
2022-01-04 17:24:44 +0900

    bookcontroller test

--- src/main/java/com/ezen/spg/cotroller/BookController.java
+++ src/main/java/com/ezen/spg/cotroller/BookController.java
@@ -4,5 +4,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookController {
-
+	// hihi ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ
+	// 그렇구나 수정하는 건 파란색
+	
+	public String orderss() {
+		return "";
+	}
 }
