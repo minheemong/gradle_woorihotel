f8678a134a3881c340e097cf80dfc4a5d74f1104
mong <mong@DESKTOP-S41GB3I>
2022-01-04 18:01:29 +0900

    member test

--- src/main/java/com/ezen/spg/dto/MemberVO.java
+++ src/main/java/com/ezen/spg/dto/MemberVO.java
@@ -1,13 +1,24 @@
 package com.ezen.spg.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class MemberVO {
 
+	@NotNull(message="아이디를 입력하세요") 
+	@NotEmpty(message="아이디를 입력하세요")
 	private String id;
-    private String pwd;     
+	@NotNull(message="비밀번호를 입력하세요") 
+	@NotEmpty(message="비밀번호를 입력하세요")
+    private String pwd; 
+	@NotNull(message="이름을 입력하세요") 
+	@NotEmpty(message="이름을 입력하세요")    
     private String name;
+	@NotNull(message="이메일을 입력하세요") 
+	@NotEmpty(message="이메일을 입력하세요")
     private String email;
     private String phone;
     private String zip_num;
