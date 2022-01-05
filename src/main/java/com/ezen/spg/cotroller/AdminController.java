20c3b4af8db1f83e8e22e5cbc7e63cd42535e458
지니 <@192.168.1.107>
2022-01-05 18:39:13 +0900

    main 페이지로 이동

--- src/main/java/com/ezen/spg/cotroller/AdminController.java
+++ src/main/java/com/ezen/spg/cotroller/AdminController.java
@@ -1,8 +1,20 @@
 package com.ezen.spg.cotroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ezen.spg.service.AdminService;
 
 @Controller
 public class AdminController {
+	
+	@Autowired
+	AdminService as;
+	
+	@RequestMapping("/a")
+	public String adminmain() {
+		return "admin/adminmain";
+	}
 
 }
