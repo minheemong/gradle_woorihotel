7ca75f11da9a5fdf54efae03541341fa3b615407
mong <mong@DESKTOP-S41GB3I>
2022-01-10 20:06:55 +0900

    admin 예약관련 로그인해야 볼 수 있게 함

--- src/main/java/com/ezen/spg/cotroller/AdminBookController.java
+++ src/main/java/com/ezen/spg/cotroller/AdminBookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ezen.spg.dto.AdminVO;
 import com.ezen.spg.dto.BookVO;
 import com.ezen.spg.dto.Paging;
 import com.ezen.spg.service.AdminBookService;
@@ -26,100 +27,97 @@ public class AdminBookController {
 	public ModelAndView adminbookList(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		
-		
 		HttpSession session = request.getSession();
-		//if(session.getAttribute("adminUser")==null) mav.setViewName("adminLogin");
-		//else {
+	    AdminVO avo = (AdminVO) session.getAttribute("loginAdmin");
+	    if(avo==null) mav.setViewName("admin/adminloginForm");
+	    else {
+			int page = 1;
+			String booknums="";
+			String indate="";
+			String outdate="";
+			String id="";
 			
-		//}
-		
-		int page = 1;
-		String booknums="";
-		String indate="";
-		String outdate="";
-		String id="";
-		
-		if(request.getParameter("booknums")!=null) {
-			booknums=request.getParameter("booknums");
-			session.setAttribute("booknums", booknums);
-		} else if(session.getAttribute("booknums")!=null) {
-			booknums=(String)session.getAttribute("booknums");
-		} else {
-			session.removeAttribute("booknums");
-			booknums="";
-		}
-		
-		if(request.getParameter("page")!=null) {
-			page=Integer.parseInt(request.getParameter("page"));
-			session.setAttribute("page", page);
-		} else if(session.getAttribute("page")!=null) {
-			page = (int)session.getAttribute("page");
-		} else {
-			page= 1;
-			session.removeAttribute("page");
-		}
-		
-		if(request.getParameter("checkins")!=null) {
-			indate=request.getParameter("checkins");
-			session.setAttribute("checkins", indate);
-		} else if(session.getAttribute("checkins")!=null) {
-			indate=(String)session.getAttribute("checkins");
-		} else {
-			session.removeAttribute("checkins");
-			indate="";
-		}
-		
-		if(request.getParameter("checkouts")!=null) {
-			outdate=request.getParameter("checkouts");
-			session.setAttribute("checkouts", outdate);
-		} else if(session.getAttribute("checkouts")!=null) {
-			outdate=(String)session.getAttribute("checkouts");
-		} else {
-			session.removeAttribute("checkouts");
-			outdate="";
-		}
-		
-		if(request.getParameter("bookid")!=null) {
-			id=request.getParameter("bookid");
-			session.setAttribute("bookid", id);
-		} else if(session.getAttribute("bookid")!=null) {
-			id=(String)session.getAttribute("bookid");
-		} else {
-			session.removeAttribute("bookid");
-			id="";
-		}
-		
-		if(request.getParameter("a")!=null) {
-			System.out.println("파라미터 a 값 : "+request.getParameter("a"));
-			session.removeAttribute("bookid");
-			id="";
-			session.removeAttribute("checkins");
-			indate="";
-			session.removeAttribute("checkouts");
-			outdate="";
-			session.removeAttribute("booknums");
-			booknums="";
-		}
-		
-		Paging paging = new Paging();
-		paging.setPage(page);
-		
-		int count=abs.getAllCount(id, booknums, indate, outdate);
-		paging.setTotalCount(count);
-		paging.paging();
-		
-		ArrayList<BookVO> booklist = abs.getAllBookList(paging,id,booknums,indate,outdate);
-		
-		mav.addObject("booklist",booklist);
-		mav.addObject("paging", paging);
-		mav.addObject("total",count);
-		mav.addObject("bookid",id);
-		mav.addObject("booknums",booknums);
-		mav.addObject("checkins",indate);
-		mav.addObject("checkouts",outdate);
-		
-		mav.setViewName("admin/book/adminbookchecklist");
-		
+			if(request.getParameter("booknums")!=null) {
+				booknums=request.getParameter("booknums");
+				session.setAttribute("booknums", booknums);
+			} else if(session.getAttribute("booknums")!=null) {
+				booknums=(String)session.getAttribute("booknums");
+			} else {
+				session.removeAttribute("booknums");
+				booknums="";
+			}
+			
+			if(request.getParameter("page")!=null) {
+				page=Integer.parseInt(request.getParameter("page"));
+				session.setAttribute("page", page);
+			} else if(session.getAttribute("page")!=null) {
+				page = (int)session.getAttribute("page");
+			} else {
+				page= 1;
+				session.removeAttribute("page");
+			}
+			
+			if(request.getParameter("checkins")!=null) {
+				indate=request.getParameter("checkins");
+				session.setAttribute("checkins", indate);
+			} else if(session.getAttribute("checkins")!=null) {
+				indate=(String)session.getAttribute("checkins");
+			} else {
+				session.removeAttribute("checkins");
+				indate="";
+			}
+			
+			if(request.getParameter("checkouts")!=null) {
+				outdate=request.getParameter("checkouts");
+				session.setAttribute("checkouts", outdate);
+			} else if(session.getAttribute("checkouts")!=null) {
+				outdate=(String)session.getAttribute("checkouts");
+			} else {
+				session.removeAttribute("checkouts");
+				outdate="";
+			}
+			
+			if(request.getParameter("bookid")!=null) {
+				id=request.getParameter("bookid");
+				session.setAttribute("bookid", id);
+			} else if(session.getAttribute("bookid")!=null) {
+				id=(String)session.getAttribute("bookid");
+			} else {
+				session.removeAttribute("bookid");
+				id="";
+			}
+			
+			if(request.getParameter("a")!=null) {
+				System.out.println("파라미터 a 값 : "+request.getParameter("a"));
+				session.removeAttribute("bookid");
+				id="";
+				session.removeAttribute("checkins");
+				indate="";
+				session.removeAttribute("checkouts");
+				outdate="";
+				session.removeAttribute("booknums");
+				booknums="";
+			}
+			
+			Paging paging = new Paging();
+			paging.setPage(page);
+			
+			int count=abs.getAllCount(id, booknums, indate, outdate);
+			paging.setTotalCount(count);
+			paging.paging();
+			
+			ArrayList<BookVO> booklist = abs.getAllBookList(paging,id,booknums,indate,outdate);
+			
+			mav.addObject("booklist",booklist);
+			mav.addObject("paging", paging);
+			mav.addObject("total",count);
+			mav.addObject("bookid",id);
+			mav.addObject("booknums",booknums);
+			mav.addObject("checkins",indate);
+			mav.addObject("checkouts",outdate);
+			
+			mav.setViewName("admin/book/adminbookchecklist");
+	    }
 		return mav;
 	}
 	
@@ -127,12 +125,19 @@ public class AdminBookController {
 	
 	
 	@RequestMapping("/adminbooklistdetail")
-	public ModelAndView adminbooklistdetail(@RequestParam("bdseq") int bdseq,
+	public ModelAndView adminbooklistdetail(HttpServletRequest request,
+			@RequestParam("bdseq") int bdseq,
 			@RequestParam("gotonum") int gotonum) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("gotonum",gotonum);
-		mav.addObject("booklist",abs.getBookDetail(bdseq));
-		mav.setViewName("admin/book/listbookcheck");
+		
+		HttpSession session = request.getSession();
+	    AdminVO avo = (AdminVO) session.getAttribute("loginAdmin");
+	    if(avo==null) mav.setViewName("admin/adminloginForm");
+	    else {
+	    	mav.addObject("gotonum",gotonum);
+			mav.addObject("booklist",abs.getBookDetail(bdseq));
+			mav.setViewName("admin/book/listbookcheck");
+	    }
 		
 		return mav;
 	}
@@ -141,14 +146,19 @@ public class AdminBookController {
 	
 	
 	@RequestMapping("/adminbookcancel")
-	public String adminbookcancel(@RequestParam("bdseq") int bdseq,
+	public String adminbookcancel(HttpServletRequest request,
+			@RequestParam("bdseq") int bdseq,
 			@RequestParam("gotonum") int gotonum) {
-		
-		String url = "redirect:/adminbookchecklist";
-		abs.adminBookCancel(bdseq);
-		
-		if(gotonum==2) url="redirect:/adminbookcancelpage";
-		
+		String url;
+		HttpSession session = request.getSession();
+	    AdminVO avo = (AdminVO) session.getAttribute("loginAdmin");
+	    if(avo==null) url = "admin/adminloginForm";
+	    else {
+			url = "redirect:/adminbookchecklist";
+			abs.adminBookCancel(bdseq);
+			
+			if(gotonum==2) url="redirect:/adminbookcancelpage";
+	    }
 		return url;
 	}
 	
@@ -158,31 +168,35 @@ public class AdminBookController {
 	@RequestMapping("/adminbookcancelpage")
 	public ModelAndView adminbookcancelpage(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
+
 		HttpSession session = request.getSession();
-		int page = 1;
-		
-		if(request.getParameter("page")!=null) {
-			page=Integer.parseInt(request.getParameter("page"));
-			session.setAttribute("page", page);
-		} else if(session.getAttribute("page")!=null) {
-			page = (int)session.getAttribute("page");
-		} else {
-			page= 1;
-			session.removeAttribute("page");
-		}
-		
-		int count =abs.getCancelAllCount();
-		Paging paging = new Paging();
-		paging.setPage(page);
-		paging.setTotalCount(count);
-		paging.paging();
-		
-		mav.addObject("booklist",abs.getAdminCancelList(paging));
-		mav.addObject("total",count);
-		mav.addObject("paging",paging);
-		
-		mav.setViewName("admin/book/adminbookcancelpage");
-		
+	    AdminVO avo = (AdminVO) session.getAttribute("loginAdmin");
+	    if(avo==null) mav.setViewName("admin/adminloginForm");
+	    else {
+			int page = 1;
+			
+			if(request.getParameter("page")!=null) {
+				page=Integer.parseInt(request.getParameter("page"));
+				session.setAttribute("page", page);
+			} else if(session.getAttribute("page")!=null) {
+				page = (int)session.getAttribute("page");
+			} else {
+				page= 1;
+				session.removeAttribute("page");
+			}
+			
+			int count =abs.getCancelAllCount();
+			Paging paging = new Paging();
+			paging.setPage(page);
+			paging.setTotalCount(count);
+			paging.paging();
+			
+			mav.addObject("booklist",abs.getAdminCancelList(paging));
+			mav.addObject("total",count);
+			mav.addObject("paging",paging);
+			
+			mav.setViewName("admin/book/adminbookcancelpage");
+	    }
 		return mav;
 	}
 	
